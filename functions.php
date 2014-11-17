504d811d77aba3427d9d5a981d133c2af97330b3
Jake Spurlock <jake_spurlock@wired.com>
2014-11-17 12:14:16 -0800

    Echo...

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ if ( !defined('ABSPATH')) exit;
 /* Add custom functions below */
 
 function add_twitter_card_support() {
-	return '<meta name="twitter:widgets:csp" content="on">';
+	echo '<meta name="twitter:widgets:csp" content="on">';
 }
 
-add_filter('wp_head', 'add_twitter_card_support');
+add_filter( 'wp_head', 'add_twitter_card_support' );
