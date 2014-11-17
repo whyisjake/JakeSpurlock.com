e54f87b556c53a07d30b559bc120bf6bd6e475ad
Jake Spurlock <jake_spurlock@wired.com>
2014-11-17 12:08:59 -0800

    Add twitter card support.

--- functions.php
+++ functions.php
@@ -3,3 +3,9 @@
 if ( !defined('ABSPATH')) exit;
 
 /* Add custom functions below */
+
+function add_twitter_card_support() {
+	return '<meta name="twitter:widgets:csp" content="on">';
+}
+
+add_filter('wp_head', 'add_twitter_card_support');
